ea6ca1bef59b4ddbb12246a21d39229d1a31ce78
dayxnight <160743101+dayxnight@users.noreply.github.com>
2026-05-05 21:33:02 +0700

    Update cache name and URLs in service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "v1_cache";
+const CACHE_NAME = "v1.2_cache";
 const urlsToCache = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/theme.css",
-    "/theme-dark.css",
-    "/icon-192.png", // Tambahkan ini
-    "/icon-512.png", // Tambahkan ini
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./theme.css",
+    "./theme-dark.css",
+    "./icon-192.png", // Tambahkan ini
+    "./icon-512.png", // Tambahkan ini
     "https://cdn.jsdelivr.net/npm/gsap@3.15/dist/gsap.min.js",
     "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
 ];
